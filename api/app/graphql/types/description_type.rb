5ca76cd075a24145db6b90c2aa7fc4730f020595
佐藤太一 <3101taichi@gmail.com>
2021-12-06 07:51:44 +0900

    docker-compose run api rails g graphql:object Description

---
+++ api/app/graphql/types/description_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class DescriptionType < Types::BaseObject
+    field :id, ID, null: false
+    field :description, String, null: true
+    field :alternative_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
