2ec45e4b491474f72a9c8fa0bf084bdc44eb417c
佐藤太一 <3101taichi@gmail.com>
2021-12-05 21:33:26 +0900

    docker-compose run api rails g graphql:object Alternative

---
+++ api/app/graphql/types/alternative_type.rb
@@ -0,0 +1,10 @@
+module Types
+  class AlternativeType < Types::BaseObject
+    field :id, ID, null: false
+    field :alternative, String, null: false
+    field :is_correct, Boolean, null: false
+    field :question_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
