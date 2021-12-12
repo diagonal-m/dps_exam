003aba9d45a4f20747e28e649524992d22abd876
佐藤太一 <3101taichi@gmail.com>
2021-12-12 18:34:38 +0900

    questionidを表示

--- front/src/components/QuestionArea.tsx
+++ front/src/components/QuestionArea.tsx
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+
+const TAG_STYLE = styled.p`
+  display: inline-block;
+  margin: 0 .1em .6em 0;
+  padding: .6em;
+  line-height: 1;
+  text-decoration: none;
+  color: #0000ee;
+  background-color: #fff;
+  border: 1px solid #0000ee;
+  margin-bottom: 0em;
+`
+
 export const QuestionArea = (props: any) => {
   const question = props.question
   const alternatives = question.randomAlternatives
@@ -5,6 +19,7 @@ export const QuestionArea = (props: any) => {
 
   return (
     <div style={{padding: "0.5em 1em", margin: "2em 0", border: "double 5px #4ec4d3", marginBottom: "0.5em"}}>
+      <TAG_STYLE>{`question id: ${question.id}`}</TAG_STYLE>
       <h2>{question.question}</h2>
       <p>{`${question.consecutive} 回連続正解中`}</p>
       {alternatives.map((alternative: {id: string, alternative: string}, i: number) => (
